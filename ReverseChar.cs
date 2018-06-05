3aa0942450e74dd804a6bda8a6054f56141ddb00
Brandon <36168001+Grover815@users.noreply.github.com>
2018-06-05 14:30:01 -0400

    Create ReverseChar.cs
    Allows any string of characters contain 2-10 characters to be reversed

---
+++ ReverseChar.cs
@@ -0,0 +1,109 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var val = Console.ReadLine();
+		var start = val.Length;
+		if (start == 2)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var output = secondc +firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 3)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var output = thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 4)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var output = fourthc + thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 5)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var fifthc = val.Substring(4,1);
+			var output = fifthc + fourthc + thirdc + secondc + firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 6)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var fifthc = val.Substring(4,1);
+			var sixthc = val.Substring(5,1);
+			var output = sixthc + fifthc + fourthc + thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 7)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var fifthc = val.Substring(4,1);
+			var sixthc = val.Substring(5,1);
+			var seventhc = val.Substring(6,1);
+			var output = seventhc + sixthc + fifthc + fourthc + thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+			if (start == 8)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var fifthc = val.Substring(4,1);
+			var sixthc = val.Substring(5,1);
+			var seventhc = val.Substring(6,1);
+			var eigthc = val.Substring(7,1);
+			var output = eigthc + seventhc + sixthc + fifthc + fourthc + thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 9)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var fifthc = val.Substring(4,1);
+			var sixthc = val.Substring(5,1);
+			var seventhc = val.Substring(6,1);
+			var eigthc = val.Substring(7,1);
+			var ninthc = val.Substring(8,1);
+			var output = ninthc + eigthc + seventhc + sixthc + fifthc + fourthc + thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+		if (start == 10)
+		{
+			var firstc = val.Substring(0,1);
+			var secondc = val.Substring(1,1);
+			var thirdc = val.Substring(2,1);
+			var fourthc = val.Substring(3,1);
+			var fifthc = val.Substring(4,1);
+			var sixthc = val.Substring(5,1);
+			var seventhc = val.Substring(6,1);
+			var eigthc = val.Substring(7,1);
+			var ninthc = val.Substring(8,1);
+			var tenthc = val.Substring(9,1);
+			var output = tenthc + ninthc + eigthc + seventhc + sixthc + fifthc + fourthc + thirdc + secondc +firstc;
+			Console.WriteLine(output);
+		}
+	}
+}
